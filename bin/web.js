7d26cf6ef711b20bf46f8f95772560ad8b0c8a26
Leonardo Matos <leomp120894@gmail.com>
2018-05-02 17:47:39 -0300

    AWS SDK API abstraction

--- bin/web.js
+++ bin/web.js
@@ -4,37 +4,35 @@
 const logger = require('./../lib/Logger.js')
 // authentication with Store API
 const auth = require('./../lib/Auth.js')
+// AWS SDK API abstraction
+const aws = require('./../lib/Aws.js')
 
 // NodeJS filesystem module
 const fs = require('fs')
+// working with file and directory paths
+const path = require('path')
 
 // Express web framework
 // https://www.npmjs.com/package/express
 const Express = require('express')
 // middleware to handle file uploads
 const multer = require('multer')
-
-// AWS SDK for S3
-const aws = require('aws-sdk')
 // extends file uploads to S3 object storage
 const multerS3 = require('multer-s3')
 
-// process.cwd() can change
-// keep initial absolute path
-let root = process.cwd()
 // read config file
-fs.readFile(root + '/config/config.json', 'utf8', (err, data) => {
+fs.readFile(path.join(__dirname, 'config/config.json'), 'utf8', (err, data) => {
   if (err) {
     // can't read config file
     throw err
   } else {
     let { port, baseUri, doSpace } = JSON.parse(data)
 
-    // set S3 endpoint to DigitalOcean Spaces
-    const spacesEndpoint = new aws.Endpoint(doSpace.datacenter + '.digitaloceanspaces.com')
-    const s3 = new aws.S3({
-      endpoint: spacesEndpoint
-    })
+    // S3 endpoint to DigitalOcean Spaces
+    const locationConstraint = doSpace.datacenter
+    const awsEndpoint = locationConstraint + '.digitaloceanspaces.com'
+    const { s3 } = aws(awsEndpoint, locationConstraint)
+
     // setup multer for file uploads
     const upload = multer({
       storage: multerS3({
